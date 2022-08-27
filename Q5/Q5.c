1adf4f2f846a856b4a0198e60e01709023a70cfc
Millena Almeida <100153774+Millenac@users.noreply.github.com>
2022-08-27 18:24:51 -0300

    Update Q5.c

--- Q5/Q5.c
+++ Q5/Q5.c
@@ -1,3 +1,12 @@
+Escreva um programa que inverta os caracteres de uma string.
+
+IMPORTANTE!
+    
+A) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+B) Evite usar funções prontas, como, por exemplo, reverse; 
+
+
+
 #include<stdio.h>
 #define t 7 //O t está recebendo o tamanho da string
 
